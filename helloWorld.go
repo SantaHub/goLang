dddc707fa6ffeaa782dbb0d980e7d2cfec1a4506
Santahub <pbaustin96@gmail.com>
2022-01-24 13:30:28 -0500

    Hello world

---
+++ helloWorld.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World !")
+}
+
+// `go fmt` -> format source code
+// `go run helloWorld.go`  -> Runs/builds the executable
+// `go build helloWorld.go` -> Create an exec
+// `go test` -> Runs the test files. 
+// `go get ` -> Fetches the packages
